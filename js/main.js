e69fd017fab05a0b9909ce0bb28efed1f3522469
Justina <justina.babrauskiene@gmail.com>
2022-10-25 12:11:34 +0300

    1 done

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let myName = "Justina";
+let surname = "Babr";
+let dateOfBirth = 1988;
+let dateToday = 2022;
+console.log("Aš esu " + myName + " " + surname + ". Man " + (dateToday - dateOfBirth) + " metai(ų).");
+//---------------------
+
